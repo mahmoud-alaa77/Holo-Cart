31a5a6cd9447cb41853cf3a299dfb973e986c085
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-01-08 10:45:11 +0200

    add splash and onBoarding images , app color class and app text styles class

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const Color primaryOrangeColor = Color(0xFFFA6E20);
+  static const Color customYellowColor = Color(0xFFFEAE35);
+  static const Color customPurpleColor = Color(0xFF8280FF);
+  static const Color customGreenColor = Color(0xFF3FBDB5);
+}
