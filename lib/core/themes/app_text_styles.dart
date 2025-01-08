4123243813435657239f7386ae4153a248fa895b
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-01-08 13:10:18 +0200

    update text style calss

--- lib/core/themes/app_text_styles.dart
+++ lib/core/themes/app_text_styles.dart
@@ -1,5 +1,15 @@
-
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTextStyles {
-  
+  static TextStyle font26W900 = TextStyle(
+    fontSize: 26.0.sp,
+    fontWeight: FontWeight.w900,
+  );
+
+ static TextStyle font15W400 = TextStyle(
+    fontSize: 15.0.sp,
+    fontWeight: FontWeight.w400,
+  );
+
 }
