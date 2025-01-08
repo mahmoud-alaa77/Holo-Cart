6e5fc10d9bea643eda30efd0d14f6e73b8855a31
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-01-08 13:09:42 +0200

    add flutter_screenutil package and use it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:holo_cart/features/home/ui/home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:holo_cart/features/on_boarding/ui/on_boarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Holo Cart',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Holo Cart',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+        ),
+        home: const OnBoardingScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
