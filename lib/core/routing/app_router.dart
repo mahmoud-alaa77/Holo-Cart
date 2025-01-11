809118f072aa728f3044fd56446ad12e1ce2a390
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-01-11 10:10:00 +0200

    change routes and add new class to add routes string

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,21 +1,22 @@
 
 
 import 'package:go_router/go_router.dart';
+import 'package:holo_cart/core/routing/routes.dart';
 import 'package:holo_cart/features/login/ui/login_screen.dart';
 import 'package:holo_cart/features/login_or_signup_guest/ui/login_signup_guest_screen.dart';
 import 'package:holo_cart/features/on_boarding/ui/on_boarding_screen.dart';
 
 final router = GoRouter(routes: [
   GoRoute(
-    path: '/onboarding',
+    path: Routes.onBoarding,
     builder: (context, state) => const OnBoardingScreen(),
   ),
   GoRoute(
-    path: '/login',
+    path: Routes.login,
     builder: (context, state) => const LoginScreen(),
   ),
   GoRoute(
-    path: '/',
+    path: Routes.mainAuth,
     builder: (context, state) => const LoginORSignupORGuestScreen(),
   ),
 ]);
