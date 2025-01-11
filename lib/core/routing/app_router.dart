347121a2138249ac0c575850c0041411c687ea56
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-01-11 11:25:32 +0200

    finish splash screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,22 +1,47 @@
-
-
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:holo_cart/core/routing/routes.dart';
 import 'package:holo_cart/features/login/ui/login_screen.dart';
 import 'package:holo_cart/features/login_or_signup_guest/ui/login_signup_guest_screen.dart';
 import 'package:holo_cart/features/on_boarding/ui/on_boarding_screen.dart';
+import 'package:holo_cart/features/splash/splash_screen.dart';
 
-final router = GoRouter(routes: [
-  GoRoute(
-    path: Routes.onBoarding,
-    builder: (context, state) => const OnBoardingScreen(),
-  ),
-  GoRoute(
-    path: Routes.login,
-    builder: (context, state) => const LoginScreen(),
-  ),
-  GoRoute(
-    path: Routes.mainAuth,
-    builder: (context, state) => const LoginORSignupORGuestScreen(),
-  ),
-]);
+final router = GoRouter(
+  routes: [
+    // Splash Route
+    GoRoute(
+      path: Routes.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
+    // Onboarding Route with Custom Slide Transition
+    GoRoute(
+      path: Routes.onBoarding,
+      name: "on_boarding",
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const OnBoardingScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0), // Slide in from the right
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          },
+        );
+      },
+    ),
+    // Login Route
+    GoRoute(
+      path: Routes.login,
+      builder: (context, state) => const LoginScreen(),
+    ),
+    // Main Auth Route
+    GoRoute(
+      path: Routes.mainAuth,
+      builder: (context, state) => const LoginORSignupORGuestScreen(),
+    ),
+  ],
+);
