1e039f1484e8687077451d31de40a2ff9c983d48
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-01-29 16:56:04 +0200

    refactor code

--- lib/features/login/ui/widgets/back_item.dart
+++ lib/features/login/ui/widgets/back_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:holo_cart/core/routing/app_router.dart';
 import 'package:holo_cart/core/routing/routes.dart';
 import 'package:holo_cart/core/themes/app_colors.dart';
 
--- lib/features/sign_up/ui/widgets/already_have_account.dart
+++ lib/features/sign_up/ui/widgets/already_have_account.dart
@@ -27,6 +27,6 @@ class AlreadyHaveAccount extends StatelessWidget {
           ),
         )
       ],
-    );;
+    );
   }
 }
