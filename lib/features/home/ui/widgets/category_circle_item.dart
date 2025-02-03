fa59b965b8c2c3c0b3ee1b31286a43dfd5a9e0f6
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-02-03 15:17:22 +0200

    refactor.............

---
+++ lib/features/home/ui/widgets/category_circle_item.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:holo_cart/core/helper/spacing.dart';
+import 'package:holo_cart/core/themes/app_colors.dart';
+import 'package:holo_cart/core/themes/app_text_styles.dart';
+
+class CategoryCircleItem extends StatelessWidget {
+  final int index;
+  final String title;
+  const CategoryCircleItem({
+    super.key,
+    required this.index,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsDirectional.only(
+          top: 6.h, bottom: 6.h, start: index == 0 ? 0 : 8.h, end: 8.h),
+      child: GestureDetector(
+        onTap: () {},
+        child: Column(
+          children: [
+            Stack(alignment: Alignment.center, children: [
+              Container(
+                width: 57.w,
+                height: 57.w,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color:
+                        AppColors.customLightGrayColor.withValues(alpha: .5)),
+              ),
+              Container(
+                width: 50.w,
+                height: 50.w,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset(
+                      "assets/images/category.jpg",
+                      fit: BoxFit.fill,
+                    )),
+              ),
+            ]),
+            verticalSpace(4),
+            Text(
+              title,
+              style: AppTextStyles.font15W500,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+List<String> categories = [
+  "Fashion",
+  "Beauty",
+  "Electronics",
+  "Home",
+  "Kitchen",
+  "Games",
+  "Sports",
+  "Travel",
+  "Toys",
+  "Health",
+  "Automotive",
+  "Beauty",
+  "Electronics",
+  "Home",
+  "Kitchen",
+  "Games",
+  "Sports",
+  "Travel",
+  "Toys",
+  "Health",
+  "Automotive",
+  "More"
+];
