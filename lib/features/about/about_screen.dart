449822492077e41803b8ff87140931f663e2f947
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-03-10 14:30:48 +0200

    الاساس بتاع ال about screen

--- lib/features/about/about_screen.dart
+++ lib/features/about/about_screen.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:holo_cart/core/helper/spacing.dart';
+import 'package:holo_cart/core/themes/app_colors.dart';
 import 'package:holo_cart/core/themes/app_text_styles.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
+  final String aboutText =
+      "Shoppe - Shopping UI kit is likely a user interface (UI) kit designed to facilitate the development of e-commerce or shopping-related applications. UI kits are collections of pre-designed elements, components, and templates that developers and designers can use to create consistent and visually appealing user interfaces.";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            "About Holo Cart",
-            style: AppTextStyles.font18W600,
-          )
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsetsDirectional.symmetric(horizontal: 16.r),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpace(24),
+              Row(
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+                  Text(
+                    "About Holo Cart",
+                    style: AppTextStyles.font20W700,
+                  ),
+                ],
+              ),
+              verticalSpace(16),
+              Text(
+                aboutText,
+                style: AppTextStyles.font16W500
+                    .copyWith(color: AppColors.customLightGrayColor),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
