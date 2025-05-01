63bbe6154307f1b0fc539e9973168a0fb4c72e72
Khaled Mohaseb <163205650+Khaled112003@users.noreply.github.com>
2025-05-01 17:02:06 +0300

    connect logic with ui in verification code

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -9,6 +9,7 @@ import 'package:holo_cart/features/checkout/ui/checkout_screen.dart';
 import 'package:holo_cart/features/checkout/ui/done_screen.dart';
 import 'package:holo_cart/features/checkout/ui/proccessing_order_screen.dart';
 import 'package:holo_cart/features/forget_password/logic/forget_password/forget_password_cubit.dart';
+import 'package:holo_cart/features/forget_password/logic/verify/verification_code_cubit.dart';
 import 'package:holo_cart/features/forget_password/ui/forget_password_screen.dart';
 import 'package:holo_cart/features/forget_password/ui/reset_password.dart';
 import 'package:holo_cart/features/forget_password/ui/verication_code_screen.dart';
@@ -154,16 +155,21 @@ final router = GoRouter(
         child: const ForgetPasswordScreen(),
       ),
     ),
-    GoRoute(
-      path: AppRoutes.verificationCode,
-      builder: (context, state) {
-        final email = state.extra as String;
-        return BlocProvider(
-          create: (context) => getIt<ForgetPasswordCubit>(),
-          child: VericationCodeScreen(email: email),
-        );
-      },
-    ),
+  GoRoute(
+  path: AppRoutes.verificationCode,
+  builder: (context, state) {
+    final email = state.extra as String;
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<ForgetPasswordCubit>()),
+        BlocProvider(create: (_) => getIt<VerificationCodeCubit>()),
+      ],
+      child: VerificationCodeScreen(email: email),
+    );
+  },
+),
+
+
     GoRoute(
       path: AppRoutes.resetPassword,
       builder: (context, state) => const ResetPassword(),
