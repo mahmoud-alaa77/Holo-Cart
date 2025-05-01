d799942a7587f12c24621dabc6c8213507041965
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-05-01 19:55:16 +0300

    similer products done

--- lib/features/product_details/ui/widgets/similar_to_cart_item.dart
+++ lib/features/product_details/ui/widgets/similar_to_cart_item.dart
@@ -5,33 +5,33 @@ import 'package:holo_cart/core/helper/spacing.dart';
 import 'package:holo_cart/core/routing/app_routes.dart';
 import 'package:holo_cart/core/themes/app_colors.dart';
 import 'package:holo_cart/core/themes/app_text_styles.dart';
+import 'package:holo_cart/features/home/data/models/get_all_products_model.dart';
 
-class  SimilarToCartItem extends StatelessWidget {
-  const  SimilarToCartItem({super.key});
+class SimilarToCartItem extends StatelessWidget {
+  final ProductData product;
+  const SimilarToCartItem({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        context.push(AppRoutes.productDetails);
+        context.push(AppRoutes.productDetails, extra: product);
       },
-      child: Container( 
-        
-       
-        margin: EdgeInsets.symmetric(horizontal: 6.w,),
+      child: Container(
+        width: 150.w,
+        margin: EdgeInsets.symmetric(horizontal: 6.w, vertical: 12.r),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16.r),
-          color:Theme.of(context).brightness == Brightness.dark
-                    ? AppColors.customBlackColor.withValues(alpha: .5)
-                    : const Color.fromARGB(255, 241, 239, 239),
-          boxShadow:[
-      BoxShadow(
-        color: Colors.black.withGreen(DateTime.august), 
-        blurRadius: 3.r, 
-       
-        offset: Offset(0, 3), 
-      ),
-    ],
+          color: Theme.of(context).brightness == Brightness.dark
+              ? AppColors.customBlackColor.withValues(alpha: .5)
+              : const Color.fromARGB(255, 241, 239, 239),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(100),
+              blurRadius: 3.r,
+              offset: const Offset(0, 2),
+            ),
+          ],
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -41,43 +41,47 @@ class  SimilarToCartItem extends StatelessWidget {
                 topLeft: Radius.circular(10.r),
                 topRight: Radius.circular(10.r),
               ),
-              child: Image.asset(
-                "assets/images/phone.png",
-                 width: 140.w,
-        height: 130.h,
-                
-                
+              child: Image.network(
+                product.mainImageUrl.toString(),
+                width: 150.w,
+                height: 130.h,
                 fit: BoxFit.fill,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Icon(Icons.error);
+                },
               ),
             ),
-           
             verticalSpace(8),
-            Text(
-              "iPhone 16 Plus",
-              style: AppTextStyles.font14W600,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            
-            Row(
-              
-              
-              children: [
-                Text(
-                  "12,000\$",
-                  style: AppTextStyles.font12W700,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                horizontalSpace(48),
-                IconButton(onPressed: () {
-                  
-                },icon: Icon(
-                  Icons.favorite_border_sharp,)
-                  
-                )
-              ],
-            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name.toString(),
+                    style: AppTextStyles.font14W600,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "${product.basePrice.toString()}\$",
+                        style: AppTextStyles.font12W700,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      horizontalSpace(48),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border_sharp,
+                          ))
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
