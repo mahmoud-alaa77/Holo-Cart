3554a42a4012fbe609db503522d8e4ee98707666
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2025-05-01 20:45:30 +0300

    change app icon

---
+++ lib/features/product_details/ui/widgets/information_product_details.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:holo_cart/core/helper/spacing.dart';
+// import 'package:holo_cart/core/themes/app_colors.dart';
+// import 'package:holo_cart/core/themes/app_text_styles.dart';
+// import 'package:holo_cart/core/widgets/button_item.dart';
+// import 'package:holo_cart/core/widgets/shimmer_loading_contianer.dart';
+// import 'package:holo_cart/features/home/data/models/get_all_products_model.dart';
+// import 'package:holo_cart/features/product_details/logic/cubit/get_product_colors_cubit.dart';
+// import 'package:holo_cart/features/product_details/ui/widgets/product_colors.dart';
+// import 'package:holo_cart/features/product_details/ui/widgets/silimilar_to_list_view.dart';
+// import 'package:readmore/readmore.dart';
+
+// class InformationProductDetails extends StatelessWidget {
+//   final ProductData product;
+//   const InformationProductDetails({super.key, required this.product});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ;
+//   }
+// }
