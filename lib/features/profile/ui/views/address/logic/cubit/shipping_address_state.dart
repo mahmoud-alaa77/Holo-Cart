ed6405818cded550d645eb47acea1c7a28b27fb5
Khaled Mohaseb <163205650+Khaled112003@users.noreply.github.com>
2025-05-31 02:18:05 +0300

    create repo& cubit & state

---
+++ lib/features/profile/ui/views/address/logic/cubit/shipping_address_state.dart
@@ -0,0 +1,17 @@
+part of 'shipping_address_cubit.dart';
+
+@immutable
+sealed class ShippingAddressState {}
+
+final class ShippingAddressInitial extends ShippingAddressState {}
+final class ShippingAddressLoading extends ShippingAddressState {}
+final class ShippingAddressSuccess extends ShippingAddressState {
+  final ApiResponse response;
+
+  ShippingAddressSuccess(this.response);
+}
+final class ShippingAddressFailure extends ShippingAddressState {
+  final String error;
+
+  ShippingAddressFailure(this.error);
+}
