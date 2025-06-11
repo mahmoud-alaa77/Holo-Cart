3462977c118b2d2d074d9a7e14f9c59777c26f2a
Khaled Mohaseb <163205650+Khaled112003@users.noreply.github.com>
2025-06-11 22:17:17 +0300

    create getShippingAddress fun i api service create Get Address Response Model

--- lib/features/profile/ui/views/address/data/models/create_shipping_address/shipping_address_response.dart
+++ lib/features/profile/ui/views/address/data/models/create_shipping_address/shipping_address_response.dart
@@ -1,5 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
-part 'shipping_address_response.g.dart';
+part '../shipping_address_response.g.dart';
 
 @JsonSerializable()
 class ShippingAddressResponse {
