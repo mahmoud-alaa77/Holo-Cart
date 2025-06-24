a93351f560cec3f14ea9523c9492e535bc6899ef
Khaled Mohaseb <163205650+Khaled112003@users.noreply.github.com>
2025-06-24 03:09:49 +0300

    add env

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,10 +7,7 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
-<<<<<<< HEAD
 #include <flutter_inappwebview_windows/flutter_inappwebview_windows_plugin_c_api.h>
-=======
->>>>>>> d6c21e1cc44a19f3462726afee05908d3cffb91e
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 #include <rive_common/rive_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
@@ -18,11 +15,8 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-<<<<<<< HEAD
   FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
-=======
->>>>>>> d6c21e1cc44a19f3462726afee05908d3cffb91e
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
   RivePluginRegisterWithRegistrar(
